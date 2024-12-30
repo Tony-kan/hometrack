56debbe4ed5c4dd0b998d47a568d68c8d74c2c1e
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-12-30 23:42:05 +0200

    Home :Integrated with database,rendering dynamic data from databases,Database:created functions to getProperties and search for a specific  property from the database

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -2,28 +2,34 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import { Models } from "react-native-appwrite";
+import { descriptions } from "jest-config";
 
 interface CardProps {
+  item: Models.Document;
   onPress: () => void;
 }
 
-export const FeaturedCard = ({ onPress }: CardProps) => {
+export const FeaturedCard = ({
+  item: { image, name, address, rating, price },
+  onPress,
+}: CardProps) => {
   return (
     <TouchableOpacity onPress={onPress} className="flex  flex-col items-start w-60 h-80 relative">
-      <Image source={images.japan} className="size-full  rounded-2xl" />
+      <Image source={{ uri: image }} className="size-full  rounded-2xl" />
       <Image source={images.cardGradient} className="size-full rounded-2xl absolute bottom-0" />
       <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5 gap-2">
         <Image source={icons.star} className="size-3.5" />
-        <Text className="text-base font-rubik-bold text-primary-300 ml-0.5">4.4</Text>
+        <Text className="text-base font-rubik-bold text-primary-300 ml-0.5">{rating}</Text>
       </View>
       <View className="flex flex-col items-start  absolute bottom-5 inset-x-5">
         <Text className="text-xl font-rubik-extraBold text-white" numberOfLines={1}>
-          Modern Apartment
+          {name}
         </Text>
-        <Text className="text-base font-rubik text-white mt-2">22 W 15th St,New York</Text>
+        <Text className="text-base font-rubik text-white mt-2">{address}</Text>
 
         <View className="flex flex-row items-center  justify-between w-full">
-          <Text className="text-xl font-rubik-extraBold text-white mt-2">$2,500</Text>
+          <Text className="text-xl font-rubik-extraBold text-white mt-2">{`$ ${price}`}</Text>
           <Image source={icons.heart} className="size-5" />
         </View>
       </View>
@@ -31,7 +37,7 @@ export const FeaturedCard = ({ onPress }: CardProps) => {
   );
 };
 
-export const Card = ({ onPress }: CardProps) => {
+export const Card = ({ item: { image, name, address, rating, price }, onPress }: CardProps) => {
   return (
     <TouchableOpacity
       onPress={onPress}
@@ -39,17 +45,17 @@ export const Card = ({ onPress }: CardProps) => {
     >
       <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full gap-2">
         <Image source={icons.star} className="size-2.5" />
-        <Text className="text-sm font-rubik-bold text-primary-300 ml-0.5">4.4</Text>
+        <Text className="text-sm font-rubik-bold text-primary-300 ml-0.5">{rating}</Text>
       </View>
 
-      <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+      <Image source={{ uri: image }} className="w-full h-40 rounded-lg" />
 
       <View className="flex flex-col mt-2">
-        <Text className="text-base font-rubik-bold text-black-300">Cozy Studio</Text>
-        <Text className="text-xs font-rubik text-black ">22 W 15th St,New York</Text>
+        <Text className="text-base font-rubik-bold text-black-300">{name}</Text>
+        <Text className="text-xs font-rubik text-black ">{address}</Text>
 
         <View className="flex flex-row items-center  justify-between mt-2">
-          <Text className="text-base font-rubik-bold text-primary-300">$2,500</Text>
+          <Text className="text-base font-rubik-bold text-primary-300">{`$ ${price}`}</Text>
           <Image source={icons.heart} className="size-5" />
         </View>
       </View>
