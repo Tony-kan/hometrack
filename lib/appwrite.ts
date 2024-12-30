83decb6b1f7a30e555642163d1949bb3d3858718
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-12-30 22:24:22 +0200

    Database : created databases,collections:properties,galleries,agents,reviews,and collections attributes,call them in appwrite config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import { Account, Avatars, Client, Databases, OAuthProvider } from "react-native-appwrite";
 import * as Linking from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
 
@@ -6,6 +6,11 @@ export const config = {
   platform: "com.brainingx.hometrack",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
 
 export const client = new Client();
@@ -14,12 +19,13 @@ client.setEndpoint(config.endpoint!).setProject(config.projectId!).setPlatform(c
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+export const databases = new Databases(client);
 
 export async function login() {
   try {
     const redirectUri = Linking.createURL("/");
 
-    const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUri);
+    const response = account.createOAuth2Token(OAuthProvider.Google, redirectUri);
 
     if (!response) throw new Error("Create OAuth2 Token Failed.");
 
